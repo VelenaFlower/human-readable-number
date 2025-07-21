c4c56c127462fbf7384a5cb7fe612a69a5ef76e7
Elena Yanchenko <elenayanchenko512@gmail.com>
2025-07-21 14:48:56 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const units = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  const digit = number % 10;
+
+  if (number < 20) {
+    return units[number];
+  }
+  if (number < 100) {
+    return (
+      tens[Math.floor(number / 10) - 2] + (digit ? ` ${units[digit]}` : '')
+    );
+  }
+  if (number < 1000) {
+    return `${units[Math.floor(number / 100)]} hundred${
+      number % 100 === 0 ? '' : ` ${toReadable(number % 100)}`
+    }`;
+  }
+  return undefined;
 };
